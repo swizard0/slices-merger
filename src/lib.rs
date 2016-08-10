46be227f9c039a72287bd4685a34c5281a16d30b
Alexey Voznyuk <me@swizard.info>
2016-08-10 08:34:33 +0300

    make it easier

--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,41 @@
+use std::ops::Deref;
 use std::cmp::Ordering;
-use std::ops::{Deref, DerefMut};
-
-pub trait Reducer<T> {
-    type Error;
-
-    fn reduce(&self, existing: &mut T, incoming: T) -> Result<(), Self::Error>;
-}
-
-impl<T, E, F> Reducer<T> for F where F: Fn(&mut T, T) -> Result<(), E> {
-    type Error = E;
-
-    fn reduce(&self, existing: &mut T, incoming: T) -> Result<(), E> {
-        (*self)(existing, incoming)
-    }
-}
 
 enum Switch { StateInit, StateA, StateB, }
 
-pub struct SlicesMergerReduce<T, R> {
-    reducer: R,
+pub struct SlicesMerger<T> {
     switch: Switch,
     union_buf_a: Vec<T>,
     union_buf_b: Vec<T>,
 }
 
-pub type SlicesMergerUniq<T> = SlicesMergerReduce<T, fn(&mut T, T) -> Result<(), ()>>;
-
-pub struct SlicesMerger<T>(SlicesMergerUniq<T>);
-
 impl<T> SlicesMerger<T> {
     pub fn new() -> SlicesMerger<T> {
-        SlicesMerger(SlicesMergerReduce::new(SlicesMerger::uniq_merge))
-    }
-
-    pub fn with_capacity(capacity: usize) -> SlicesMerger<T> {
-        SlicesMerger(SlicesMergerReduce::with_capacity(SlicesMerger::uniq_merge, capacity))
-    }
-
-    pub fn from(init_vec: Vec<T>) -> SlicesMerger<T> {
-        SlicesMerger(SlicesMergerReduce::from(init_vec, SlicesMerger::uniq_merge))
-    }
-
-    pub fn from_with_capacity(init_vec: Vec<T>, capacity: usize) -> SlicesMerger<T> {
-        SlicesMerger(SlicesMergerReduce::from_with_capacity(init_vec, SlicesMerger::uniq_merge, capacity))
-    }
-
-    pub fn add<I>(&mut self, items: I) where T: PartialOrd + PartialEq, I: Iterator<Item = T> {
-        match self.0.add(items) {
-            Ok(()) => (),
-            Err(()) => unreachable!(),
-        }
-    }
-
-    pub fn finish(self) -> Vec<T> {
-        self.0.finish()
-    }
-
-    fn uniq_merge(_existing: &mut T, _incoming: T) -> Result<(), ()> {
-        Ok(())
-    }
-}
-
-impl<T> Deref for SlicesMerger<T> {
-    type Target = SlicesMergerUniq<T>;
-
-    fn deref(&self) -> &SlicesMergerUniq<T> {
-        &self.0
-    }
-}
-
-impl<T> DerefMut for SlicesMerger<T> {
-    fn deref_mut(&mut self) -> &mut SlicesMergerUniq<T> {
-        &mut self.0
-    }
-}
-
-impl<T, R> SlicesMergerReduce<T, R> {
-    pub fn new(reducer: R) -> SlicesMergerReduce<T, R> {
-        SlicesMergerReduce {
-            reducer: reducer,
+        SlicesMerger {
             switch: Switch::StateInit,
             union_buf_a: Vec::new(),
             union_buf_b: Vec::new(),
         }
     }
 
-    pub fn with_capacity(reducer: R, capacity: usize) -> SlicesMergerReduce<T, R> {
-        SlicesMergerReduce {
-            reducer: reducer,
+    pub fn with_capacity(capacity: usize) -> SlicesMerger<T> {
+        SlicesMerger {
             switch: Switch::StateInit,
             union_buf_a: Vec::with_capacity(capacity),
             union_buf_b: Vec::with_capacity(capacity),
         }
     }
 
-    pub fn from(init_vec: Vec<T>, reducer: R) -> SlicesMergerReduce<T, R> {
-        SlicesMergerReduce {
-            reducer: reducer,
+    pub fn from(init_vec: Vec<T>) -> SlicesMerger<T> {
+        SlicesMerger {
             switch: Switch::StateB,
             union_buf_b: Vec::with_capacity(init_vec.len() + 1),
             union_buf_a: init_vec,
         }
     }
 
-    pub fn from_with_capacity(init_vec: Vec<T>, reducer: R, capacity: usize) -> SlicesMergerReduce<T, R> {
-        SlicesMergerReduce {
-            reducer: reducer,
+    pub fn from_with_capacity(init_vec: Vec<T>, capacity: usize) -> SlicesMerger<T> {
+        SlicesMerger {
             switch: Switch::StateB,
             union_buf_b: Vec::with_capacity(capacity),
             union_buf_a: init_vec,
@@ -117,31 +47,28 @@ impl<T, R> SlicesMergerReduce<T, R> {
         self.union_buf_a.clear();
     }
 
-    pub fn add<I>(&mut self, items: I) -> Result<(), R::Error> where
-        T: PartialOrd,
-        R: Reducer<T>,
-        I: Iterator<Item = T>
+    pub fn add<I>(&mut self, items: I) where T: PartialOrd, I: Iterator<Item = T>
     {
-        self.add_by(items, |a, b| a.partial_cmp(b))
+        self.add_by(items, |a, b| a.partial_cmp(b), |_, _| ())
     }
 
-    pub fn add_by<I, F>(&mut self, items: I, comp: F) -> Result<(), R::Error> where
-        R: Reducer<T>,
+    pub fn add_by<I, FC, FR>(&mut self, items: I, comp: FC, reduce: FR) where
         I: Iterator<Item = T>,
-        F: Fn(&T, &T) -> Option<Ordering>
+        FC: Fn(&T, &T) -> Option<Ordering>,
+        FR: FnMut(&mut T, T)
     {
         match self.switch {
             Switch::StateInit => {
                 self.switch = Switch::StateB;
-                union_into::<_, ::std::vec::Drain<T>, _, _, _>(&mut self.union_buf_a, None, items, comp, &mut self.reducer)
+                union_into::<_, ::std::vec::Drain<T>, _, _, _>(&mut self.union_buf_a, None, items, comp, reduce)
             },
             Switch::StateA => {
                 self.switch = Switch::StateB;
-                union_into(&mut self.union_buf_a, Some(self.union_buf_b.drain(..)), items, comp, &mut self.reducer)
+                union_into(&mut self.union_buf_a, Some(self.union_buf_b.drain(..)), items, comp, reduce)
             },
             Switch::StateB => {
                 self.switch = Switch::StateA;
-                union_into(&mut self.union_buf_b, Some(self.union_buf_a.drain(..)), items, comp, &mut self.reducer)
+                union_into(&mut self.union_buf_b, Some(self.union_buf_a.drain(..)), items, comp, reduce)
             },
         }
     }
@@ -154,7 +81,7 @@ impl<T, R> SlicesMergerReduce<T, R> {
     }
 }
 
-impl<T, R> Deref for SlicesMergerReduce<T, R> {
+impl<T> Deref for SlicesMerger<T> {
     type Target = [T];
 
     fn deref(&self) -> &[T] {
@@ -165,11 +92,11 @@ impl<T, R> Deref for SlicesMergerReduce<T, R> {
     }
 }
 
-fn union_into<T, IS, II, F, R>(target: &mut Vec<T>, source_it: Option<IS>, mut src_b: II, comp: F, reducer: &mut R) -> Result<(), R::Error> where
+fn union_into<T, IS, II, FC, FR>(target: &mut Vec<T>, source_it: Option<IS>, mut src_b: II, comp: FC, mut reduce: FR) where
     IS: Iterator<Item = T>,
     II: Iterator<Item = T>,
-    F: Fn(&T, &T) -> Option<Ordering>,
-    R: Reducer<T>
+    FC: Fn(&T, &T) -> Option<Ordering>,
+    FR: FnMut(&mut T, T)
 {
     target.clear();
     match source_it {
@@ -181,7 +108,7 @@ fn union_into<T, IS, II, F, R>(target: &mut Vec<T>, source_it: Option<IS>, mut s
                 surface =
                     match surface {
                         (None, None) =>
-                            break,
+                            return,
                         (None, Some(entry_b)) => {
                             target.push(entry_b);
                             (None, src_b.next())
@@ -192,7 +119,7 @@ fn union_into<T, IS, II, F, R>(target: &mut Vec<T>, source_it: Option<IS>, mut s
                         },
                         (Some(mut entry_a), Some(entry_b)) => match comp(&entry_a, &entry_b) {
                             Some(Ordering::Equal) => {
-                                try!(reducer.reduce(&mut entry_a, entry_b));
+                                reduce(&mut entry_a, entry_b);
                                 target.push(entry_a);
                                 (src_a.next(), src_b.next())
                             },
@@ -209,13 +136,11 @@ fn union_into<T, IS, II, F, R>(target: &mut Vec<T>, source_it: Option<IS>, mut s
             }
         },
     }
-
-    Ok(())
 }
 
 #[cfg(test)]
 mod test {
-    use super::{SlicesMerger, SlicesMergerReduce};
+    use super::SlicesMerger;
 
     #[test]
     fn basic() {
@@ -238,12 +163,14 @@ mod test {
 
     #[test]
     fn reduce() {
-        let mut merger =
-            SlicesMergerReduce::new(|&mut (_, ref mut total): &mut (_, _), (_, inc)| { *total += inc; Ok::<(), ()>(()) });
+        fn comp(&(ref a, _): &(isize, isize), &(ref b, _): &(isize, isize)) -> Option<::std::cmp::Ordering> { a.partial_cmp(b) }
+        fn reduce(&mut (_, ref mut total): &mut (isize, isize), (_, inc): (isize, isize)) { *total += inc }
+
+        let mut merger = SlicesMerger::new();
         assert_eq!(merger.len(), 0);
-        merger.add_by([1, 3, 4, 5, 7].iter().cloned().map(|v| (v, 1)), |&(ref a, _), &(ref b, _)| a.partial_cmp(b)).unwrap();
+        merger.add_by([1, 3, 4, 5, 7].iter().cloned().map(|v| (v, 1)), comp, reduce);
         assert_eq!(&merger[..], &[(1, 1), (3, 1), (4, 1), (5, 1), (7, 1)]);
-        merger.add_by([2, 4, 5, 6, 7, 8, 9].iter().cloned().map(|v| (v, 1)), |&(ref a, _), &(ref b, _)| a.partial_cmp(b)).unwrap();
+        merger.add_by([2, 4, 5, 6, 7, 8, 9].iter().cloned().map(|v| (v, 1)), comp, reduce);
         assert_eq!(&merger[..], &[(1, 1), (2, 1), (3, 1), (4, 2), (5, 2), (6, 1), (7, 2), (8, 1), (9, 1)]);
     }
 
